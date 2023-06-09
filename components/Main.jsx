9f8c84f7c5de9f37bb1fd8a472d998e65fe188c4
Matthew82-def <matt.herrera82@gmail.com>
2023-06-09 00:52:26 -0500

    main move

--- components/Main.jsx
+++ components/Main.jsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 const Main = () => {
   return (
     <div id="home" className="w-full h-screen text-center">
-      <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
+      <div className="max-w-[1550px] w-full h-full mx-auto p-2 flex justify-center items-center">
         <div>
           <p className="uppercase text-sm tracking-widest text-cyan-600">
             Together we can build a creative world!
