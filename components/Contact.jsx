39de48f04de72b3007c3b2ef0d28f66d97e04f55
Matthew82-def <matt.herrera82@gmail.com>
2023-06-19 18:40:23 -0500

    updated contact form to support API

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import connect from "../public/assets/connect.png";
 import { FaGithub, FaLinkedinIn } from "react-icons/fa";
@@ -8,6 +8,61 @@ import { HiOutlineChevronDoubleUp } from "react-icons/hi";
 import Link from "next/link";
 
 const Contact = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+    subject: "",
+    phoneNumber: "",
+  });
+
+  const [formSuccess, setFormSuccess] = useState(false);
+  const [formSuccessMessage, setFormSuccessMessage] = useState("");
+
+  const handleInput = (e) => {
+    const fieldName = e.target.name;
+    const fieldValue = e.target.value;
+
+    setFormData((prevState) => ({
+      ...prevState,
+      [fieldName]: fieldValue,
+    }));
+  };
+
+  const submitForm = (e) => {
+    // We don't want the page to refresh
+    e.preventDefault();
+
+    const formURL = e.target.action;
+    const data = new FormData();
+
+    // Turn our formData state into data we can use with a form submission
+    Object.entries(formData).forEach(([key, value]) => {
+      data.append(key, value);
+    });
+
+    // POST the data to the URL of the form
+    fetch(formURL, {
+      method: "POST",
+      body: data,
+      headers: {
+        accept: "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setFormData({
+          name: "",
+          email: "",
+          message: "",
+          subject: "",
+          phoneNumber: "",
+        });
+
+        setFormSuccess(true);
+        setFormSuccessMessage(data.message);
+      });
+  };
   return (
     <div id="contact" className="w-full lg:h-screen">
       <div className="max-w-[1240px] m-auto px-2 py-16 w-full ">
@@ -71,52 +126,90 @@ const Contact = () => {
 
           <div className="col-span-3 w-full h-full shadow-xl shadow-blue-400 rounded-xl lg:p-4">
             <div className="p-4">
-              <form>
-                <div className="grid md:grid-cols-2 gap-4 w-full py-2">
-                  <div className="flex flex-col">
-                    <label className="uppercase text-sm py-2">Name</label>
-                    <input
-                      className="border-2 rounded-lg p-3 flex border-blue-300"
-                      type="text"
-                    />
+              {formSuccess ? (
+                <div>{formSuccessMessage}</div>
+              ) : (
+                <form
+                  action="https://api.avernix.com/api/mail/form?mailToken=t7L4BvkB7"
+                  method="post"
+                  onSubmit={submitForm}
+                >
+                  <div className="grid md:grid-cols-2 gap-4 w-full py-2">
+                    <div className="flex flex-col">
+                      <label className="uppercase text-sm py-2" for="name">
+                        Name
+                      </label>
+                      <input
+                        className="border-2 rounded-lg p-3 flex border-blue-300"
+                        onChange={handleInput}
+                        value={formData.name}
+                        name="name"
+                        type="text"
+                        required
+                      />
+                    </div>
+                    <div className="flex flex-col">
+                      <label
+                        className="uppercase text-sm py-2"
+                        for="phoneNumber"
+                      >
+                        Phone Number
+                      </label>
+                      <input
+                        className="border-2 rounded-lg p-3 flex border-blue-300"
+                        onChange={handleInput}
+                        value={formData.phoneNumber}
+                        name="phoneNumber"
+                        type="text"
+                        required
+                      />
+                    </div>
                   </div>
-                  <div className="flex flex-col">
-                    <label className="uppercase text-sm py-2">
-                      Phone Number
+
+                  <div className="flex flex-col py-2">
+                    <label className="uppercase text-sm py-2" for="email">
+                      Email
                     </label>
                     <input
                       className="border-2 rounded-lg p-3 flex border-blue-300"
-                      type="text"
+                      onChange={handleInput}
+                      value={formData.email}
+                      name="email"
+                      type="email"
+                      required
                     />
                   </div>
-                </div>
-
-                <div className="flex flex-col py-2">
-                  <label className="uppercase text-sm py-2">Email</label>
-                  <input
-                    className="border-2 rounded-lg p-3 flex border-blue-300"
-                    type="email"
-                  />
-                </div>
-                <div className="flex flex-col py-2">
-                  <label className="uppercase text-sm py-2">Subject</label>
-                  <input
-                    className="border-2 rounded-lg p-3 flex border-blue-300"
-                    type="text"
-                  />
-                </div>
-                <div className="flex flex-col py-2">
-                  <label className="uppercase text-sm py-2">Message</label>
-                  <textarea
-                    className="border-2 rounded-lg p-3 border-blue-300"
-                    rows="20"
-                    name="message"
-                  ></textarea>
-                </div>
-                <button className="w-full p-4 text-blue-100 mt-4">
-                  Send Message
-                </button>
-              </form>
+                  <div className="flex flex-col py-2">
+                    <label className="uppercase text-sm py-2" for="subject">
+                      Subject
+                    </label>
+                    <input
+                      className="border-2 rounded-lg p-3 flex border-blue-300"
+                      onChange={handleInput}
+                      value={formData.subject}
+                      name="subject"
+                      type="text"
+                      required
+                    />
+                  </div>
+                  <div className="flex flex-col py-2">
+                    <label className="uppercase text-sm py-2" for="message">
+                      Message
+                    </label>
+                    <textarea
+                      className="border-2 rounded-lg p-3 border-blue-300"
+                      onChange={handleInput}
+                      value={formData.message}
+                      rows="20"
+                      name="message"
+                      required
+                    ></textarea>
+                  </div>
+                  <button className="w-full p-4 text-blue-100 mt-4">
+                    Send Message
+                  </button>
+                </form>
+              )}
             </div>
           </div>
         </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,27 @@
-import Image from 'next/image'
-import Head from 'next/head'
+import Image from "next/image";
+import Head from "next/head";
 // import { Inter } from 'next/font/google'
-import Navbar from '../components/Navbar'
-import Main from '../components/Main'
-import About from '../components/About'
-import Skills from '../components/Skills'
-import Projects from '../components/Projects'
-import Contact from '../components/Contact'
-
-
+import Navbar from "../components/Navbar";
+import Main from "../components/Main";
+import About from "../components/About";
+import Skills from "../components/Skills";
+import Projects from "../components/Projects";
+import Contact from "../components/Contact";
 
 // const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  
   return (
     <div>
-    <Head>
-     <title>Matt the Software Developer</title>
-    </Head>
-    <Navbar />
-    <Main />
-    <About />
-    <Skills/>
-    <Projects />
-    <Contact />
-   
+      <Head>
+        <title>Matt the Software Developer</title>
+      </Head>
+      <Navbar />
+      <Main />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
     </div>
-  )
+  );
 }
