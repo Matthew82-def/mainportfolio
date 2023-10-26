97e312b991012d25238e80a0b91b303d68a6f6b5
Matthew82-def <84039703+Matthew82-def@users.noreply.github.com>
2023-10-25 21:32:37 -0500

    project change

--- pages/nonpro.jsx
+++ pages/nonpro.jsx
@@ -18,7 +18,7 @@ const nonpro = () => {
         />
         <div className="absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2">
           <h2 className="py-2">NonproAdco Webpage</h2>
-          <h3>Next JS / Tailwind</h3>
+          <h3>Next JS / Tailwind.CSS / API Integration</h3>
         </div>
       </div>
 
